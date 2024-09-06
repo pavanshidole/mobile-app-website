54b038a7ec9175edf850d3e0e422838d4ff1b1b7
pavan shidole <pavanshidole@gmail.com>
2024-09-06 11:39:28 +0530

    static -  mobile app website

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+var person={
+    fname:"jhon",
+    lname:"doe",
+    age:50,
+    nativeplace:"pune",
+    profile:"MEAN stack",
+    child:{
+        fname:"jen",
+        lname:"doe",
+        age:24,
+        nativeplace:"latur",
+        porfile:"java developer",
+        grandchild:{
+            fname:"june",
+            lname:"doe",
+            age:5,
+            nativeplace:"latur"
+        }
+    }
+};
+
+console.log(person);
